0c2e2543010b361b2fffda65097707be153c2637
arun06001 <arun06001@E-PW0234KE>
2023-09-15 10:31:18 +0200

    Add project files.

---
+++ Uppgift 2.4/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace Uppgift_2_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv in lönen för 3 personer på företaget separerade med kommatecken");
+            string svar = Console.ReadLine();
+
+            byte index1 = (byte)svar.IndexOf(",");
+            byte index2 = (byte)svar.IndexOf(",",(index1+1));
+
+            int lön1 = int.Parse(svar[..index1]);
+            int lön2 = int.Parse(svar[(index1+1)..index2]);
+            int lön3 = int.Parse(svar[(index2 + 1)..]);
+
+            Console.WriteLine($"Medellönen är {(lön1+lön2+lön3)/3} kronor.");
+
+            Console.ReadKey();
+        }
+    }
+}
